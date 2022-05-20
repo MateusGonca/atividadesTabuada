3a806ce8e763acda0c2003ef91c1e65925d8db83
Bestd <Bestd@DESKTOP-7NQDND0>
2022-05-19 21:22:31 -0300

    Adicionar arquivos de projeto.

---
+++ Repetição_1/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repetição_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ||====================================================||
+            // ||**** AUTOR: RENAN LIMA | RA: 6321300 ***************||
+            // ||**** DATA CRIAÇÃO: 01.05.2022 **********************||
+            // ||**** MATÉRIA: LÓGICA DE PROGRAMAÇÃO ****************||
+            // ||**** SEMESTRE: 1 ***********************************||
+            // ||====================================================||
+
+            ////// ATIVIDADE 1 DJALMA
+            ///// Mostrar tabuadas de 1 número escolhido pelo usuário.
+
+            //int numero, contador = 1;
+
+            //Console.Write("Digite o número que deseja saber a tabuada: ");
+
+            //numero = int.Parse(Console.ReadLine());
+
+            //if (numero <= 10)
+            //{
+
+            //    while (contador <= 10)
+            //    {
+
+            //        Console.WriteLine(numero + " x " + contador + " = " + numero * contador);
+            //        contador++;
+
+            //    }
+            //}
+
+            //else {
+
+            //    Console.WriteLine("Número inválido, por favor digite um número de 0 a 10. ");  
+            //}
+
+            //    Console.ReadKey();
+
+
+            //======================================================================================
+
+            //////ATIVIDADE 2 DJALMA
+            ///// Usar a mesma ideia e mostrar de forma decrescente.
+
+            //int numero, contador = 10;
+
+            //Console.Write("Digite o número que deseja saber a tabuada: ");
+
+            //numero = int.Parse(Console.ReadLine());
+
+            //if (numero >= 1)
+            //{
+
+            //    while (contador >= 1)
+            //    {
+
+            //        Console.WriteLine(numero + " x " + contador + " = " + numero * contador);
+            //        contador--;
+
+            //    }
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("Número inválido, por favor digite um número de 0 a 10. ");
+            //}
+
+            //Console.ReadKey();
+
+            //======================================================================================
+
+            ////ATIVIDADE 3 DJALMA
+            //// Modificar o código e mostrar tabuadas de 1 a 10 em sequência.
+
+            //int mult = 0, produto, op = 0;
+
+            //while (mult <= 10)
+            //{
+            //    Console.WriteLine("--------------------");
+            //    Console.WriteLine("TABUADA DO " + mult);
+            //    Console.WriteLine("--------------------");
+
+            //    while (op <= 10)
+            //    {
+
+            //        produto = mult * op;
+            //        Console.WriteLine(mult + " x " + op + " = " + produto);
+            //        op = op + 1;
+
+            //    }
+
+            //    mult = mult + 1;
+            //    op = op - 10;
+            //}
+
+            //Console.ReadKey();
+
+        }
+    }
+}
+
+            
+   
+
